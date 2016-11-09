5f7525908089bfbe53d0a2fcd0c07dba456427ef
opussf <github@zz9-za.com>
2016-11-09 02:06:22 -0800

    Using md not lg.  Design more for the smaller screen. Still need to clean up signup.

--- WebSrc/signup.php
+++ WebSrc/signup.php
@@ -24,28 +24,48 @@ if (isset($_POST["username"])) {
 }
 
 ?>
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/currency.css">
+	<script src="js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Currency Simulator</title>
+</head>
+<body onload=document.login.username.focus()>
+<div class="container-fluid">
+<div class="row head">
+<div class="col-md-6 col-md-offset-3">
+<h1><center>Currency Simulator sign up</center></h1>
+<?=$errorString?>
+</div> <!-- col -->
+</div> <!-- row head -->
+<div class="row">
+<div class="col-sm-4 col-sm-offset-2">
+<div class="row"><div class="col-md-12">
+Please create a username and password.<br/>
+I'm not securing the passwords, they are sent in the clear.<br/>
+Use the simplest password you want (or none).<br/>
+Try to hack your friends, earn currency for them.
+</div></div> <!-- row -->
+</div> <!-- col -->
+<div class="col-sm-4">
+<form name=signup action="signup.php" method="post">
+<div class="row">
+<div class="col-sm-3">Username:</div><div class="col-sm-9"><input type="text" name="username"></div>
+</div> <!-- row -->
+<div class="row">
+<div class="col-sm-3">Password:</div><div class="col-sm-9"><input type="password" name="password"></div>
+</div> <!-- row -->
+<div class="row">
+<div class="col-sm-offset-3 col-sm-9"><input type="submit" name="submit" value="Sign Up"></div>
+</div> <!-- row -->
 
-<html><head><title>Currency Simulator</title></head>
-	<body onload=document.login.username.focus()>
-		<h1><center>Currency Simulator sign up</center></h1>
-		<?=$errorString?>
-		<hr>
-		<table border=0 align=left width=75%>
-			<tr><td align=left><table border=0> <!-- Left side menu bar -->
-				<tr><td>Please create a username and password.</td></tr>
-				<tr><td>I'm not securing the passwords, they are sent in the clear.</td></tr>
-				<tr><td>Use the simplist password you want (or none).</td></tr>
-				<tr><td>Try to hack your friends, earn currency for them.</td></tr>
-			</table>
-			<td align=right><table border=0>	<!-- Login Form -->
-				<form name=signup action="signup.php" method="post">
-					<tr><th>Username:</th><td><input type="text" name="username"></td></tr>
-					<tr><th>Password:</th><td><input type="password" name="password"></td></tr>
-					<tr><td></td><td align=center><input type="submit" name="submit" value="Sign Up"></td></tr>
-				</form>
-			</table>
-		</table>
+</div> <!-- row -->
 
-	</body>
+</div> <!-- container -->
+
+</body>
 </html>
 
